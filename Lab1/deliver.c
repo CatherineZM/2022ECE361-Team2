68731fc6ae515a8843216b580636ed670a213f83
Amily <amily.wu@mail.utoronto.ca>
2022-10-12 21:00:02 -0400

    completed lab2 now can transfer file

--- Lab1/deliver.c
+++ Lab1/deliver.c
@@ -7,14 +7,26 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <ctype.h>
+#include <time.h>
 
 #define MAXBUFLEN 100
+#define MAXFILELEN 9
+#define MAXFILESTRLEN MAXFILELEN+MAXBUFLEN
 
 //define functions
 int read_client_input();
+int cal_total_frag();
+void prepare_file_str();
+void send_check();
 
 int main(int argc, char *argv[])
 {
+	//define varibales
+	clock_t start_t, end_t; double RTT;
+	int total_frag = 0; int frag_no = 0;
+	char file_name[MAXBUFLEN]; char file_str[MAXFILESTRLEN]; 
+	
+	
 	// check if input is valid
 	if(argc != 3) {
 		fprintf(stderr,"deliver: Invalid input number, expect 3 but input %d\n", argc);
@@ -65,23 +77,21 @@ int main(int argc, char *argv[])
 	
 	// promte message for a user to enter command and save input message
 	while(1) {
-		printf("Enter your input message in format ftp <file name>: \n");
+		printf("Enter your input in format ftp <filename.txt>: \n");
 		fgets(message, MAXBUFLEN, stdin);
-		printf("deliver: your input message is %s", message);
-		if(read_client_input(message, MAXBUFLEN))
+		// printf("deliver: your input message is %s", message);
+		if(read_client_input(message, MAXBUFLEN, &file_name))
 			return 0;
-		if(!read_client_input(message, MAXBUFLEN))
+		if(!read_client_input(message, MAXBUFLEN, &file_name))
 			break;
 	}
 	
 	// client send "ftp" to server
-	char * handshake = "ftp"; 
+	char* handshake = "ftp"; 
 	int sendMsg;
+	start_t = clock(); //RTT starts
 	sendMsg = sendto(sockfd, handshake, strlen(handshake), 0, servinfo->ai_addr, servinfo->ai_addrlen);
-	if(sendMsg < 0) {
-		fprintf(stderr, "deliver: error when sending message%n\n", sendMsg);
-		return 1; 
-	}
+	send_check(sendMsg);
 
 	freeaddrinfo(servinfo);
 
@@ -93,10 +103,28 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "deliver: message received is invalid\n");
 		return 1;
     }
-
 	// if server's reply is "yes", tell user file transfer can start
 	if(strcmp(reply, "yes") == 0) {
+		fprintf(stdout, "Server responds OK\n");
 		fprintf(stdout, "A file transfer can start\n");
+	}
+	else {
+		fprintf(stdout, "Refused by server, unable to start file transfer\n");
+	}
+	end_t = clock(); //RTT ends
+	RTT = (double)(end_t - start_t) / CLOCKS_PER_SEC;
+  	printf("The round-trip time (RTT) = %.3f ms\n", RTT*1000);
+
+	total_frag = cal_total_frag(&file_name);
+	printf("File will sent in %d packets\n", total_frag);
+
+	for(frag_no = 1; frag_no <= total_frag; frag_no++) {
+		prepare_file_str(total_frag, frag_no, &file_name, &file_str);
+		char* p = &file_str[0]; 
+		sendMsg = sendto(sockfd, p, MAXFILESTRLEN, 0, servinfo->ai_addr, servinfo->ai_addrlen);
+		send_check(sendMsg);
+		printf("packet<%d> sent\n", frag_no);
+		// freeaddrinfo(servinfo);
 	}
 
     close(sockfd);
@@ -104,7 +132,13 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
-int read_client_input(char* input, int input_length) {
+void send_check(int sendMsg) {
+	if(sendMsg < 0) {
+		fprintf(stderr, "deliver: error when sending message%n\n", sendMsg);
+	}
+}
+
+int read_client_input(char* input, int input_length, char file_name[MAXBUFLEN]) {
 	int word_count = 0; 
 	int start = 0; 
 	int space = 0;
@@ -170,7 +204,69 @@ int read_client_input(char* input, int input_length) {
 		return 1;
 	}
 
+	strcpy(file_name, input_file);
 	//no error, ready to send ftp
 	return 0;
 }
 
+//calculate total frags in file
+int cal_total_frag(char file_name[MAXBUFLEN]) {
+	char open_file[MAXBUFLEN];
+	FILE* text_file = NULL;
+    int len;
+	strcpy(open_file, file_name);
+	text_file = fopen(open_file, "r");
+	if(text_file == NULL) {
+		printf("Failed to read file <%s>\n", open_file);
+		return -1;
+	}
+	fseek(text_file, 0, SEEK_END);
+	len = ftell(text_file);
+	if(len % MAXFILELEN)
+		len = len / MAXFILELEN + 1;
+	else
+		len = len / MAXFILELEN;
+
+	return len;
+}
+
+void prepare_file_str(int total_frag, int frag_no, char file_name[MAXBUFLEN], char file_str[MAXFILESTRLEN]) {
+	char open_file[MAXBUFLEN];
+	FILE* text_file = NULL;
+	char text[MAXFILELEN+1]; //'\0' at last
+    int len; int len_end;
+	char str_buf[MAXBUFLEN];
+	strcpy(open_file, file_name);
+	text_file = fopen(open_file, "r");
+	if(text_file == NULL) 
+		printf("Failed to prepare packet <%d>\n", frag_no);
+
+	//read str from file
+	fseek(text_file, (frag_no-1)*MAXFILELEN, SEEK_SET);
+	if(total_frag == frag_no) {
+		fseek(text_file, 0, SEEK_END);
+		len_end = ftell(text_file);
+		fseek(text_file, (frag_no-1)*MAXFILELEN, SEEK_SET);
+		len = ftell(text_file);
+		len = len_end - len;
+	}
+	else
+		len = MAXFILELEN;
+	fread(text, sizeof(char), len, text_file);
+
+	//make up file_str
+	sprintf(file_str, "%d", total_frag);
+	strcat(file_str, ":");
+	sprintf(str_buf, "%d", frag_no);
+	strcat(str_buf, ":");
+	strcat(file_str, str_buf);
+	sprintf(str_buf, "%d", len);
+	strcat(str_buf, ":");
+	strcat(file_str, str_buf);
+	strcat(file_str, file_name);
+	strcat(file_str, ":");
+	text[len] = '\0';
+	strcat(file_str, text);
+
+	fclose(text_file);
+}
--- Lab1/server.c
+++ Lab1/server.c
@@ -6,11 +6,21 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <ctype.h>
+#include <stddef.h>
 
 #define MAXBUFLEN 100
+#define MAXFILELEN 9
+#define MAXFILESTRLEN MAXFILELEN+MAXBUFLEN
+
+//call funtion
+int create_file();
 
 int main(int argc, char *argv[])
 {
+	//define variable
+	char received_str[MAXFILESTRLEN];
+
 	// check if the input is valid
 	if(argc != 2) {
 		fprintf(stderr,"server: Invalid input number, expect 2 but input %d\n", argc);
@@ -60,7 +70,7 @@ int main(int argc, char *argv[])
 
 	freeaddrinfo(servinfo);
 
-	printf("Finished make and bind a socket. Start Listening to port %s \n", portInput);
+	printf("Finished make and bind a socket.\nStart Listening to port %s...\n", portInput);
 
 	// Initialize client socket information
 	int numbytes;
@@ -71,34 +81,110 @@ int main(int argc, char *argv[])
 	client_addr_len = sizeof client_addr;
 
 	// receive message from client
-	numbytes = recvfrom(sockfd, buf, MAXBUFLEN-1, 0,(struct sockaddr *)&client_addr, &client_addr_len);
-	printf("server: message received from client: %s\n", buf);
-	if(numbytes < 0) {
-		fprintf(stderr, "server: message received is invalid\n");
-		return 1;
-    }
-
-	// check message content
-	int cmd_check = strcmp(buf, "ftp");
-	int sendMsg;
-
-	// if message received is "ftp", reply "yes"
-	if(cmd_check == 0) {
-		sendMsg = sendto(sockfd, "yes", strlen("yes"), 0, (struct sockaddr *) &client_addr, client_addr_len);
-		if(sendMsg == -1) {
-			fprintf(stderr, "server: error when sending message%d \n", sendMsg);
-			exit(1);
-        }
-	// if message received is not "ftp", reply "no"
-    } else {
-		sendMsg = sendto(sockfd, "no", strlen("no"), 0, (struct sockaddr *) &client_addr, client_addr_len);
-		if(sendMsg == -1) {
-			fprintf(stderr, "server: error when sending message%d \n", sendMsg);
-			exit(1);
+	while(1) {
+		numbytes = recvfrom(sockfd, buf, MAXBUFLEN-1, 0,(struct sockaddr *)&client_addr, &client_addr_len);
+		printf("server: message received from client: %s\n", buf);
+		if(numbytes < 0) {
+			fprintf(stderr, "server: message received is invalid\n");
+			return 1;
 		}
-    }
+
+		// check message content
+		int cmd_check = strcmp(buf, "ftp");
+		int sendMsg;
+
+		// if message received is "ftp", reply "yes"
+		if(cmd_check == 0) {
+			sendMsg = sendto(sockfd, "yes", strlen("yes")+1, 0, (struct sockaddr *) &client_addr, client_addr_len);
+			if(sendMsg == -1) {
+				fprintf(stderr, "server: error when sending message%d \n", sendMsg);
+				// exit(1);
+			}
+		// if message received is not "ftp", reply "no"
+		} else {
+			sendMsg = sendto(sockfd, "no", strlen("no"), 0, (struct sockaddr *) &client_addr, client_addr_len);
+			if(sendMsg == -1) {
+				fprintf(stderr, "server: error when sending message%d \n", sendMsg);
+				exit(1);
+			}
+		}
+
+		//file transfer starts
+		printf("Start receiving file...\n");
+		int file_created = 1;
+		while(file_created) {
+			numbytes = recvfrom(sockfd, received_str, MAXFILESTRLEN, 0,(struct sockaddr *)&client_addr, &client_addr_len);
+			if(numbytes < 0) {
+				fprintf(stderr, "server: file received is invalid\n");
+				return 1;
+			}
+			if(create_file(&received_str))
+				file_created = 0;
+		}
+		
+		printf("File is created\n\nListening to port again...\n");
+	}
 
     close(sockfd);
 
 	return 0;
+}
+
+int create_file(char received_str[MAXFILESTRLEN]) {
+	char *p = &received_str[0];
+	char *p_cpy = p; char *p_move = p;
+	long num = 0; int num_index = 0;
+	ptrdiff_t bytes_index = 0;
+	ptrdiff_t bytes_len = 0;
+	int total_frag = 0; int frag_no = 0; int size = 0;
+	char file_name[MAXBUFLEN]; char file_txt[MAXFILELEN+1]; //for '\0'
+
+	//handle msg to abstract struct info from received file str
+	while(*p_move) {
+		if(*p_move == ':') {
+			num_index++;
+			if(num_index == 1)
+				total_frag = strtol(p_cpy, &p_cpy, 10);
+			else if(num_index == 2)
+				frag_no = strtol(p_cpy, &p_cpy, 10);
+			else if(num_index == 3)
+				size = strtol(p_cpy, &p_cpy, 10);
+			else if(num_index == 4) {
+				bytes_index = p_cpy - p;
+				bytes_len = p_move - p_cpy;
+				strncpy(file_name, received_str+abs(bytes_index), abs(bytes_len));
+				file_name[abs(bytes_len)] = '\0';
+			}
+			p_cpy = p_move+1;
+			p_move++;
+		}
+		else 
+			p_move++;
+	}
+	bytes_index = p_cpy - p;
+	bytes_len = p_move - p_cpy;
+	strncpy(file_txt, received_str+abs(bytes_index), abs(bytes_len));
+	file_txt[abs(bytes_len)] = '\0';
+	// printf("total frag = %d, frag_no = %d, size = %d, file name = %s, file text = %s\n", 
+	// total_frag, frag_no, size, file_name, file_txt);
+
+	//since server&client are the same, create new_file.txt instead
+	strcpy(file_name, "newfile.txt");
+	file_name[11] = '\0';
+
+	//create file
+	FILE *fp = NULL;
+	fp = fopen(file_name,"a");
+	if (fp == NULL) {
+        printf("Error opening the file <%s>\n", file_name);
+    }
+    // write to the text file
+	fprintf(fp, "%s", file_txt);
+	fclose(fp);
+
+	printf("packet<%d> is done\n", frag_no);
+	if(frag_no == total_frag)
+		return 1;
+	else
+		return 0;
 }
